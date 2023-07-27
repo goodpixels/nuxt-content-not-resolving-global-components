c7d43c06909c6b3616e3a018b09c5e903ef366e5
John <john@localhost>
2023-07-27 20:29:22 +0100

    adding menu links

--- app.vue
+++ app.vue
@@ -1,4 +1,8 @@
 <template>
+  <ul>
+    <li><NuxtLink to="/">Regular nuxt page</NuxtLink></li>
+    <li><NuxtLink to="/content-test">Content page</NuxtLink></li>
+  </ul>
   <div>
     <NuxtPage />
   </div>
